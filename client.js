5cb241f0f8e2a23fca3ca971871f8d77932e52b0
Sameer Khan <sameerk_786@hotmail.com>
2020-12-03 07:58:54 +0000

    created separate file for client

---
+++ client.js
@@ -0,0 +1,28 @@
+
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+
+    
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  // challenge: update the connect function to also attach a event handler 
+  // handle incoming data and console.log it for the player
+
+  // data is happening when the client recieves a message from the server
+  conn.on('data', (message) => {
+    console.log(message);
+
+  });
+
+  return conn;
+}
+
+module.exports  = {connect};
